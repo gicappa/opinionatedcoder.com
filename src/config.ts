c3f62b1a812cd0bcee012a24c30e00956364dc9f
Gian Carlo Pace <giancarlo.pace@hyland.com>
2023-12-29 19:14:02 +0100

    chore: imported astro based website

---
+++ src/config.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'Opinionated Coder | Gian Carlo Pace\'s Personal Project and Blog Website';
+export const SITE_DESCRIPTION = 'A personal site to share my projects, blog posts, and ideas.';
---
+++ src/content/blog/opinionated-coder.md
@@ -0,0 +1,9 @@
+---
+title: "Opinionated Coder Kick-off"
+description: "What is opinionated coder"
+pubDate: "Dec 28 2023"
+heroImage: "/opinionated-coder.webp"
+tags: ["kick-off"]
+---
+
+Test
